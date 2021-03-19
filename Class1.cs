40bd0dd48a1e4adc11dc4f631f67cd880b64cdc2
shraddha kapse <shraddha.kapse@gmail.com>
2021-03-19 11:40:20 +0000

    Added code for delimeter

--- Class1.cs
+++ Class1.cs
@@ -9,10 +9,11 @@ namespace PracticeSample
     
         public static int Add(string numbers){
             int sum=0;
+            char[] delimiters = GetDelimiters(numbers);
             var negativenumbers = new List<int>();
             if(numbers.Length==0)
             return sum;
-            var myList = numbers.Split(new Char [] {',' , '\n' ,';'}).Select(s => int.Parse(s));
+            var myList = numbers.Split(delimiters).Select(s => int.Parse(s));
             foreach (var item in myList){
                 if(item<0){
                     try{
@@ -20,13 +21,27 @@ namespace PracticeSample
                         throw new Exception("Its a negative number");   
                     }catch(Exception e){
                         Console.WriteLine(e.Message + " "  + String.Join(", ", negativenumbers));
+                        continue;
                     }
-                } else{ 
+                }if(item < 1000){
                     sum =sum+item;
                 }
                 
             };
             return sum;
         }
+        public static char[] GetDelimiters(string numbers)
+        {
+            var delimiters = new List<char> { ',', '\n' };
+            if(numbers.StartsWith("//"))
+            {
+                var delimiterDeclaration = numbers.Split('\n').First();
+                char  delimiter = delimiterDeclaration.Substring(2,1).Single();
+                delimiters.Add(delimiter);                
+            }
+ 
+            return delimiters.ToArray();
+        }
+
     }
 }
